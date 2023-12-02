30c5192b548a819aed8e9eacbb9b03279ee38c58
anonymus1234-cloud <84515544+anonymus1234-cloud@users.noreply.github.com>
2023-12-02 00:48:57 -0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,27 @@
+var number1=Number(prompt("enter first number"))
+var number2=Number(prompt("enter second number"))
+var add=number1+number2
+document.getElementById("add").innerHTML="sum of two numbers are  "+add
+var subtract=number1-number2
+document.getElementById("sub").innerHTML="difference of two numbers are  "+subtract
+var multiply=number1*number2
+document.getElementById("ply").innerHTML="Product of two numbers are  "+multiply
+var divide=number1/number2
+document.getElementById("div").innerHTML="Quotient of two numbers are  "+divide
+var a=(number1==number2)
+document.getElementById("comp").innerHTML="Comparison of if two numbers are identical is: "+a
+var b=(number1===number2)
+document.getElementById("yorn").innerHTML="check if two characters are not numbers is: "+b
+var c=(number1!=number2)
+document.getElementById("fort").innerHTML="Comparison of if two numbers are not identical is : "+c
+var d=(number1>number2)
+document.getElementById("grorls").innerHTML="Comparison of if first digit is bigger than second digit is:  "+d
+var e=(number1<number2)
+document.getElementById("lsorgr").innerHTML="Comparison of if second digit is bigger than first digit is:   "+e
+
+
+
+
+
+
+
